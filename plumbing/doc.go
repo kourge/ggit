23cd23088105328313af5b35eab8afdb80cfe6a4
Wilson Lee <kourge@gmail.com>
2014-02-25 23:37:06 -0800

    Introduce the plumbing package

---
+++ plumbing/doc.go
@@ -0,0 +1,5 @@
+// The plumbing package contains functions that perform low-level Git tasks
+// (referred to as "plumbing") that involve manipulating Git objects on a the
+// file system.
+package plumbing
+
