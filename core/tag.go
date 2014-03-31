0d3ca92d14cccbc5c9dec6aeaa0651f55b520650
Wilson Lee <kourge@gmail.com>
2014-03-30 21:59:07 -0700

    Add new core type Tag

---
+++ core/tag.go
@@ -0,0 +1,159 @@
+package core
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"strings"
+	"unicode"
+)
+
+// A Tag is a Git object type that points to a Commit. There are two kinds of
+// tags in Git: a lightweight tag that is simply a ref with a name that points
+// to the SHA-1 checksum of a commit, and an annotated tag that is a Git object
+// containing a tagger, a timestamp, and a tag message. This type represents
+// the latter kind.
+type Tag struct {
+	object     sha1field
+	objectType string
+	name       string
+	tagger     *AuthorTime
+	message    string
+
+	buffer []byte
+}
+
+var _ Object = &Tag{}
+
+// NewTag returns a new Tag with the given attributes. The objectType is almost
+// always "commit".
+func NewTag(
+	object Sha1,
+	objectType string,
+	name string,
+	tagger AuthorTime,
+	message string,
+) *Tag {
+	return &Tag{sha1field{object}, objectType, name, &tagger, message, nil}
+}
+
+func (tag *Tag) Object() Sha1 {
+	return tag.object.Sha1
+}
+
+func (tag *Tag) ObjectType() string {
+	return tag.objectType
+}
+
+func (tag *Tag) Name() string {
+	return tag.name
+}
+
+func (tag *Tag) Tagger() AuthorTime {
+	return *tag.tagger
+}
+
+func (tag *Tag) Message() string {
+	return tag.message
+}
+
+func (tag *Tag) Type() string {
+	return "tag"
+}
+
+func (tag *Tag) Size() int {
+	if len(tag.buffer) == 0 {
+		tag.load()
+	}
+	return len(tag.buffer)
+}
+
+// Reader returns an io.Reader that yields this annotated tag in a serialized
+// format.
+func (tag *Tag) Reader() io.Reader {
+	fields := fieldslice{
+		{"object", &tag.object},
+		{"type", &StringCoder{tag.objectType}},
+		{"tag", &StringCoder{tag.name}},
+		{"tagger", tag.tagger},
+	}
+
+	readers := append(fields.Readers(), []io.Reader{
+		bytes.NewReader([]byte("\n\n")),
+		strings.NewReader(tag.message),
+		bytes.NewReader([]byte{'\n'}),
+	}...)
+
+	return io.MultiReader(readers...)
+}
+
+func (tag *Tag) load() {
+	buffer, err := ioutil.ReadAll(tag.Reader())
+	if err != nil {
+		Die(err)
+	}
+	tag.buffer = buffer
+}
+
+// Decode reads from an io.Reader, attempting to decode the stream as
+// a serialized Tag object. If any part of the stream is improperly formatted,
+// an error is returned.
+func (tag *Tag) Decode(reader io.Reader) error {
+	lines, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return err
+	}
+
+	parts := bytes.SplitN(lines, []byte("\n\n"), 2)
+	if len(parts) != 2 {
+		return Errorf("received %d parts while decoding tag", len(parts))
+	}
+
+	fieldBytes, message := parts[0], parts[1]
+	fields, err := fieldsliceDecode(bytes.NewReader(fieldBytes))
+	if err != nil {
+		return err
+	}
+
+	if err := tag.loadFields(fields); err != nil {
+		return err
+	}
+
+	tag.message = strings.TrimRightFunc(string(message), unicode.IsSpace)
+	tag.buffer = lines
+	return nil
+}
+
+func (tag *Tag) loadFields(fields fieldslice) error {
+	for _, field := range fields {
+		var err error
+		s := field.Value
+
+		switch field.Name {
+		case "object":
+			v := &sha1field{}
+			err = v.Decode(s.Reader())
+			tag.object = *v
+		case "type":
+			v := &StringCoder{}
+			err = v.Decode(s.Reader())
+			tag.objectType = v.string
+		case "tag":
+			v := &StringCoder{}
+			err = v.Decode(s.Reader())
+			tag.name = v.string
+		case "tagger":
+			v := &AuthorTime{}
+			err = v.Decode(s.Reader())
+			tag.tagger = v
+		default:
+			Die(Errorf("unrecognized tag field %s", field.Name))
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ core/tag_test.go
@@ -0,0 +1,126 @@
+package core
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+var (
+	_fixtureTagObject Sha1 = Sha1{
+		0x6b, 0x6f, 0x8b, 0x56, 0x6e, 0xf3, 0x24, 0x5f, 0x5b, 0x25,
+		0xd0, 0x3c, 0x61, 0xb2, 0xaf, 0x0a, 0x1f, 0x55, 0x30, 0x1e,
+	}
+	_fixtureTagObjectType string     = "commit"
+	_fixtureTagName       string     = "v4.1.0.rc2"
+	_fixtureTagTagger     AuthorTime = NewAuthorTime(
+		"David Heinemeier Hansson", "david@loudthinking.com", 1395778247, 1*3600,
+	)
+	_fixtureTagMessage string = "v4.1.0.rc2 release"
+	_fixtureTag        *Tag   = NewTag(
+		_fixtureTagObject,
+		_fixtureTagObjectType,
+		_fixtureTagName,
+		_fixtureTagTagger,
+		_fixtureTagMessage,
+	)
+	_fixtureTagString string = `object 6b6f8b566ef3245f5b25d03c61b2af0a1f55301e
+type commit
+tag v4.1.0.rc2
+tagger David Heinemeier Hansson <david@loudthinking.com> 1395778247 +0100
+
+v4.1.0.rc2 release
+`
+)
+
+func TestTag_Type(t *testing.T) {
+	var actual string = _fixtureTag.Type()
+	var expected string = "tag"
+
+	if actual != expected {
+		t.Errorf("tag.Type() = %v, want %v", actual, expected)
+	}
+}
+
+func TestTag_Size(t *testing.T) {
+	var actual int = _fixtureTag.Size()
+	var expected int = len(_fixtureTagString)
+
+	if actual != expected {
+		t.Errorf("tag.Size() = %v, want %v", actual, expected)
+	}
+}
+
+func TestTag_Reader(t *testing.T) {
+	var actual []byte
+	var expected []byte = []byte(_fixtureTagString)
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(_fixtureTag.Reader())
+	actual = buffer.Bytes()
+
+	if !bytes.Equal(actual, expected) {
+		t.Error("tag.Reader() did not generate same byte sequence")
+	}
+}
+
+func TestTag_Decode(t *testing.T) {
+	var actual *Tag = &Tag{}
+	var expected *Tag = _fixtureTag
+
+	reader := bytes.NewBuffer([]byte(_fixtureTagString))
+	err := actual.Decode(reader)
+
+	if err != nil {
+		t.Errorf("tag.Decode() returned error %v", err)
+	}
+
+	if !reflect.DeepEqual(*actual, *expected) {
+		t.Error("tag.Decode() produced %v, want %v", actual, expected)
+	}
+}
+
+func TestTag_Object(t *testing.T) {
+	var actual Sha1 = _fixtureTag.Object()
+	var expected Sha1 = _fixtureTagObject
+
+	if actual != expected {
+		t.Errorf("tag.Object() = %v, want %v", actual, expected)
+	}
+}
+
+func TestTag_ObjectType(t *testing.T) {
+	var actual string = _fixtureTag.ObjectType()
+	var expected string = _fixtureTagObjectType
+
+	if actual != expected {
+		t.Errorf("tag.ObjectType() = %v, want %v", actual, expected)
+	}
+}
+
+func TestTag_Name(t *testing.T) {
+	var actual string = _fixtureTag.Name()
+	var expected string = _fixtureTagName
+
+	if actual != expected {
+		t.Errorf("tag.Name() = %v, want %v", actual, expected)
+	}
+}
+
+func TestTag_Tagger(t *testing.T) {
+	var actual AuthorTime = _fixtureTag.Tagger()
+	var expected AuthorTime = _fixtureTagTagger
+
+	if !actual.Equal(expected) {
+		t.Errorf("tag.Tagger() = %v, want %v", actual, expected)
+	}
+}
+
+func TestTag_Message(t *testing.T) {
+	var actual string = _fixtureTag.Message()
+	var expected string = _fixtureTagMessage
+
+	if actual != expected {
+		t.Errorf("tag.Message() = %v, want %v", actual, expected)
+	}
+}
