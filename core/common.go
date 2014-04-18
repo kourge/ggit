040a44e4c89ef365cd5df0b4919febdcbc09a4eb
Wilson Lee <kourge@gmail.com>
2014-04-18 00:11:00 -0700

    Reorganize docs for core package

--- core/common.go
+++ core/common.go
@@ -1,5 +1,3 @@
-// The core package contains all the low-level primitives of Git objects and
-// provides ways to encode and decode them to and from byte streams.
 package core
 
 import (
---
+++ core/doc.go
@@ -0,0 +1,3 @@
+// The core package includes all the low-level primitives of Git objects and
+// provides ways to encode and decode them to and from byte streams.
+package core
